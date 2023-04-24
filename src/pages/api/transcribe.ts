2c31aa7c3a51079eb1bbf68272cb4eb6efd26989
Dragos <dragos@duckly.com>
2023-04-24 21:59:17 +0200

    Raise body limit from 1mb -> 10mb

--- src/pages/api/transcribe.ts
+++ src/pages/api/transcribe.ts
@@ -1,8 +1,16 @@
-const { Deepgram } = require("@deepgram/sdk");
+const { Deepgram } = require('@deepgram/sdk')
 const fs = require('fs')
 
 const deepgram = new Deepgram(process.env.DEEPGRAM_API_KEY)
 const googleApiKey = process.env.GOOGLE_CLOUD_API_KEY
+
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb',
+    },
+  },
+}
 
 export default async function (req, res) {
   if (req.method !== 'POST') {
@@ -16,22 +24,19 @@ export default async function (req, res) {
     return res.status(400).json({ error: 'Missing body.' })
   }
 
-  const googleCloudRes = await fetch(
-    `https://speech.googleapis.com/v1/speech:recognize?key=${googleApiKey}`,
-    {
-      method: 'POST',
-      body: body
-    }
-  )
+  const googleCloudRes = await fetch(`https://speech.googleapis.com/v1/speech:recognize?key=${googleApiKey}`, {
+    method: 'POST',
+    body: body,
+  })
 
-  const googleCloudJson = await googleCloudRes.json();
+  const googleCloudJson = await googleCloudRes.json()
 
   // const transcription = await deepgram.transcription.preRecorded({
   //   buffer: Buffer.from(body.audio.content, 'base64'),
   //   mimetype: 'audio/wav'
   // }, {
   //   punctuate: true,
-	// 	// model: 'nova',
+  // 	// model: 'nova',
   //   model: 'general',
   //   language: 'es',
   // })
