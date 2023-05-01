7cbd9dd211f9c5a3e830a1b9dde0c245474cacb9
Bogdan <bogdan1850@gmail.com>
2023-05-01 23:17:57 +0200

    Fix TextArea for Mobile

--- src/components/MainBox.js
+++ src/components/MainBox.js
@@ -57,7 +57,7 @@ export default function MainBox({ query, setQuery, handleSubmitButton, gettingRe
                 aria-disabled={gettingResponse}
                 onChange={(e) => setQuery(e.target.value)}
                 value={query}
-                className="resize-none flex-auto rounded-md border-0 bg-white/5 px-2.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                className="resize-none flex-auto rounded-md border-0 bg-white/5 px-2.5 py-2 text-white shadow-sm ring-1 ring-inset lg:h-16 h-32 ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 placeholder={placeholder}
               />
               <div className="items-center justify-center flex">
